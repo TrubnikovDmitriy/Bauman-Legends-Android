11986d9a02bc94c4a031cf2581d5635cc0f0129f
Dmitriy Trubnikov <trubnikovdv@yandex-team.ru>
2020-09-11 17:16:36 +0300

    Create onboarding team screen

---
+++ feature-team/src/main/java/dv/trubnikov/legends/feature_team/presentation/screens/onboarding/TeamOnBoardingViewModel.kt
@@ -0,0 +1,7 @@
+package dv.trubnikov.legends.feature_team.presentation.screens.onboarding
+
+import androidx.lifecycle.ViewModel
+
+class TeamOnBoardingViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
