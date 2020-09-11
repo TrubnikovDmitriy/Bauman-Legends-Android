f7d9203ff0f7981c3ac08b7f30a27f1a3fa1263f
Dmitriy Trubnikov <trubnikovdv@yandex-team.ru>
2020-09-12 02:53:25 +0300

    Create team screen

---
+++ feature-team/src/main/java/dv/trubnikov/legends/feature_team/presentation/screens/create/TeamCreateDialogViewModel.kt
@@ -0,0 +1,7 @@
+package dv.trubnikov.legends.feature_team.presentation.screens.create
+
+import androidx.lifecycle.ViewModel
+
+class TeamCreateDialogViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
